0d74cd37f18d550eb8f82ec28888350f63f226c1
Olivia <olivia.elfving@hotmail.com>
2021-10-24 18:15:24 +0200

    Connected to Bridge status API

--- src/components/Bridge.js
+++ src/components/Bridge.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from 'react';
 import BridgeSvg from './BridgeSvg'
 
 function Bridge() {
+
+    const [raise, setRaise] = useState(false);
+
+    useEffect(() => {
+        getBridgeStatus();
+    }, []);
+
+    const getBridgeStatus = async () => {
+        const response = await fetch('https://n400itshfa000002-development-apim.azure-api.net/RoadSignals?subscription-key=ce34027e127f4566b931cdfcc5136f0f');
+        const { status } = await response.json();
+        if (status === "Open") {
+            setRaise(false);
+        }
+        else {
+            setRaise(true);
+        }
+    }
+
     return (
-        <BridgeSvg />
+        <BridgeSvg raise={raise} />
     )
 }
 
