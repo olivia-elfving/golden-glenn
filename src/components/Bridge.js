1ce1a0f73c85d9cc28bbd2cdf8f06e9754843331
Olivia Elfving <68814404+olivia-elfving@users.noreply.github.com>
2022-04-08 14:42:43 +0200

    Error handling

--- src/components/Bridge.js
+++ src/components/Bridge.js
@@ -13,13 +13,18 @@ function Bridge() {
     }, []);
 
     const getBridgeStatus = async () => {
-        const response = await fetch('https://n400itshfa000002-development-apim.azure-api.net/RoadSignals?subscription-key=ce34027e127f4566b931cdfcc5136f0f');
-        const { status } = await response.json();
-        if (status === "Open") {
-            setRaise(false);
+        try {
+            const response = await fetch('https://n400itshfa000002-development-apim.azure-api.net/RoadSignals?subscription-key=ce34027e127f4566b931cdfcc5136f0f');
+            const { status } = await response.json();
+            if (status === "Open") {
+                setRaise(false);
+            }
+            else {
+                setRaise(true);
+            }
         }
-        else {
-            setRaise(true);
+        catch(error) {
+            console.info('Bro-api:t har ännu inte släppts. Mer info; https://goteborg.se/wps/portal/start/kommun-o-politik/kommunfakta/oppna-data/oppna-data-soksida/oppna-data-datamangd#esc_entry=458&esc_context=6');
         }
     }
 
