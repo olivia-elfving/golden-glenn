13f36b6d2846f74e2327d58bb5f9cc4d9cad1b93
Joakim Johansson <johansson.joakim@gmail.com>
2022-09-26 11:24:38 +0200

    Show if connection to bridge is online

--- src/components/WebsocketOnline.js
+++ src/components/WebsocketOnline.js
@@ -2,7 +2,7 @@ import './WebsocketOnline.css';
 
 const WebsocketOnline = ({ isOnline }) => {
     return (
-        <div className='lightbeam__container'>
+        <div className={`lightbeam__container ${isOnline ? 'lightbeam__container--online' : 'lightbeam__container--connecting'}`}>
             <div className='lightbeam'></div>
             <i className='fal fa-bat websocketOnline'></i>
         </div>
