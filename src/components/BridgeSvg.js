13d9bf825ea69a28eb6068458363e06581ea3e1b
Joakim Johansson <johansson.joakim@gmail.com>
2022-10-10 09:27:27 +0200

    Less traffic

--- src/components/BridgeSvg.js
+++ src/components/BridgeSvg.js
@@ -11,81 +11,56 @@ const BridgeSvg = ({ raise }) => (
     <path id="reversePath" d="M867.517,165.712,L552.311,150.887,L315.118,151.032,L4.216,164.771" style={{ fill: "none" }} />
     <g className={`${raise ? "hide" : ""}`} id="reverse_circles" opacity="0">
       <circle id="reverse_plane" fill="#275e8a" cx="0" cy="0" r="10"  />
-      <circle id="reverse_point-1" class="point-1" fill="#275e8a" cx="0" cy="0" r="10" />
-      <circle id="reverse_point-2" class="point-2" fill="#275e8a" cx="0" cy="0" r="10" />
-      <circle id="reverse_point-3" class="point-3" fill="#275e8a" cx="0" cy="0" r="10" />
-      <circle id="reverse_point-4" class="point-1" fill="#275e8a" cx="0" cy="0" r="10" />
-      <circle id="reverse_point-5" class="point-2" fill="#275e8a" cx="0" cy="0" r="10" />
-      <circle id="reverse_point-6" class="point-3" fill="#275e8a" cx="0" cy="0" r="10" />
-      <circle id="reverse_point-7" class="point-1" fill="#275e8a" cx="0" cy="0" r="10" />
-      <circle id="reverse_point-8" class="point-2" fill="#275e8a" cx="0" cy="0" r="10" />
-      <circle id="reverse_point-9" class="point-3" fill="#275e8a" cx="0" cy="0" r="10" />
-      <circle id="reverse_point-10" class="point-1" fill="#275e8a" cx="0" cy="0" r="10" />
-      <circle id="reverse_point-11" class="point-2" fill="#275e8a" cx="0" cy="0" r="10" />
-      <circle id="reverse_point-12" class="point-3" fill="#275e8a" cx="0" cy="0" r="10" />
+      <circle id="reverse_point-1" fill="#275e8a" cx="0" cy="0" r="10" />
+      <circle id="reverse_point-2" fill="#275e8a" cx="0" cy="0" r="10" />
+      <circle id="reverse_point-3" fill="#275e8a" cx="0" cy="0" r="10" />
+      <circle id="reverse_point-4"  fill="#275e8a" cx="0" cy="0" r="10" />
+      <circle id="reverse_point-5" fill="#275e8a" cx="0" cy="0" r="10" />
+      <circle id="reverse_point-6" fill="#275e8a" cx="0" cy="0" r="10" />
+      <circle id="reverse_point-7"  fill="#275e8a" cx="0" cy="0" r="10" />
     </g>
     <g className={`${raise ? "hide" : ""}`} id="circles" opacity="0">
-      <circle id="plane" class="plane" fill="#3379af" cx="0" cy="0" r="10"  />
-      <circle id="point-1" class="point-1" fill="#3379af" cx="0" cy="0" r="10" />
-      <circle id="point-2" class="point-2" fill="#3379af" cx="0" cy="0" r="10" />
-      <circle id="point-3" class="point-2" fill="#3379af" cx="0" cy="0" r="10" />
-      <circle id="plane-4" class="plane" fill="#3379af" cx="0" cy="0" r="10"  />
-      <circle id="point-5" class="point-1" fill="#3379af" cx="0" cy="0" r="10" />
-      <circle id="point-6" class="point-2" fill="#3379af" cx="0" cy="0" r="10" />
-      <circle id="point-7" class="point-2" fill="#3379af" cx="0" cy="0" r="10" />
-      <circle id="point-8" class="point-1" fill="#3379af" cx="0" cy="0" r="10" />
-      <circle id="point-9" class="point-2" fill="#3379af" cx="0" cy="0" r="10" />
-      <circle id="point-10" class="point-2" fill="#3379af" cx="0" cy="0" r="10" />
-      <circle id="point-12" class="point-1" fill="#3379af" cx="0" cy="0" r="10" />
-      <circle id="point-13" class="point-2" fill="#3379af" cx="0" cy="0" r="10" />
-      <circle id="point-14" class="point-2" fill="#3379af" cx="0" cy="0" r="10" />
+      <circle id="plane" fill="#3379af" cx="0" cy="0" r="10"  />
+      <circle id="point-1" fill="#3379af" cx="0" cy="0" r="10" />
+      <circle id="point-2" fill="#3379af" cx="0" cy="0" r="10" />
+      <circle id="point-3" fill="#3379af" cx="0" cy="0" r="10" />
+      <circle id="plane-4" fill="#3379af" cx="0" cy="0" r="10"  />
+      <circle id="point-5" fill="#3379af" cx="0" cy="0" r="10" />
+      <circle id="point-6" fill="#3379af" cx="0" cy="0" r="10" />
+      <circle id="point-7" fill="#3379af" cx="0" cy="0" r="10" />
+      <circle id="point-8" fill="#3379af" cx="0" cy="0" r="10" />
+      <circle id="point-9" fill="#3379af" cx="0" cy="0" r="10" />
     </g>
 
 
-    <animateMotion xlinkHref="#plane" dur="10s" begin="0s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#plane" dur="10s" begin="0.2s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#planePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#point-1" dur="9.5s" begin="0.6s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#point-1" dur="9.5s" begin="1s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#planePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#point-2" dur="9s" begin="0.9s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#point-2" dur="9s" begin="1.5s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#planePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#point-3" dur="10s" begin="1.5s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#point-3" dur="9.5s" begin="2.2s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#planePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#plane-4" dur="10s" begin="4.5s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#plane-4" dur="10.2s" begin="2.9s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#planePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#point-5" dur="9.5s" begin="4s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#point-5" dur="9.5s" begin="3.5s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#planePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#point-6" dur="9s" begin="2.5s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#point-6" dur="9s" begin="3.8s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#planePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#point-7" dur="10s" begin="3s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#point-7" dur="10.3s" begin="4.5s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#planePath" />
     </animateMotion>
-
-    <animateMotion xlinkHref="#point-8" dur="11s" begin="4s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#point-8" dur="10.5s" begin="5s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#planePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#point-9" dur="9.5s" begin="5.3s" repeatCount="indefinite" rotate="auto">
-        <mpath xlinkHref="#planePath" />
-    </animateMotion>
-    <animateMotion xlinkHref="#point-10" dur="9s" begin="5.5s" repeatCount="indefinite" rotate="auto">
-        <mpath xlinkHref="#planePath" />
-    </animateMotion>
-    <animateMotion xlinkHref="#point-11" dur="10s" begin="5.5s" repeatCount="indefinite" rotate="auto">
-        <mpath xlinkHref="#planePath" />
-    </animateMotion>
-    <animateMotion xlinkHref="#point-12" dur="9.7s" begin="4.7s" repeatCount="indefinite" rotate="auto">
-        <mpath xlinkHref="#planePath" />
-    </animateMotion>
-    <animateMotion xlinkHref="#point-13" dur="12s" begin="5s" repeatCount="indefinite" rotate="auto">
-        <mpath xlinkHref="#planePath" />
-    </animateMotion>
-    <animateMotion xlinkHref="#point-14" dur="9s" begin="5.5s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#point-9" dur="9.5s" begin="5.6s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#planePath" />
     </animateMotion>
 
@@ -97,43 +72,28 @@ const BridgeSvg = ({ raise }) => (
         fill="freeze"
     />
 
-    <animateMotion xlinkHref="#reverse_plane" dur="10s" begin="2s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#reverse_plane" dur="10s" begin="0s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#reversePath" />
     </animateMotion>
     <animateMotion xlinkHref="#reverse_point-1" dur="10.5s" begin="1.3s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#reversePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#reverse_point-2" dur="11s" begin="4.6s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#reverse_point-2" dur="11s" begin="2.6s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#reversePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#reverse_point-3" dur="9.5s" begin="3.9s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#reverse_point-3" dur="9.5s" begin="2.9s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#reversePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#reverse_point-4" dur="10s" begin="1.3s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#reverse_point-4" dur="10s" begin="3.2s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#reversePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#reverse_point-5" dur="9.7s" begin="1.6s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#reverse_point-5" dur="9.7s" begin="4.6s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#reversePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#reverse_point-6" dur="10.5s" begin="3s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#reverse_point-6" dur="10.5s" begin="5s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#reversePath" />
     </animateMotion>
-    <animateMotion xlinkHref="#reverse_point-7" dur="10.5s" begin="4.4s" repeatCount="indefinite" rotate="auto">
-        <mpath xlinkHref="#reversePath" />
-    </animateMotion>
-    <animateMotion xlinkHref="#reverse_point-8" dur="11s" begin="5.6s" repeatCount="indefinite" rotate="auto">
-        <mpath xlinkHref="#reversePath" />
-    </animateMotion>
-    <animateMotion xlinkHref="#reverse_point-9" dur="12.5s" begin="4.9s" repeatCount="indefinite" rotate="auto">
-        <mpath xlinkHref="#reversePath" />
-    </animateMotion>
-    <animateMotion xlinkHref="#reverse_point-10" dur="12s" begin="2.1s" repeatCount="indefinite" rotate="auto">
-        <mpath xlinkHref="#reversePath" />
-    </animateMotion>
-    <animateMotion xlinkHref="#reverse_point-11" dur="12.7s" begin="4.5s" repeatCount="indefinite" rotate="auto">
-        <mpath xlinkHref="#reversePath" />
-    </animateMotion>
-    <animateMotion xlinkHref="#reverse_point-12" dur="12.5s" begin="5.6s" repeatCount="indefinite" rotate="auto">
+    <animateMotion xlinkHref="#reverse_point-7" dur="10.5s" begin="5.6s" repeatCount="indefinite" rotate="auto">
         <mpath xlinkHref="#reversePath" />
     </animateMotion>
 
