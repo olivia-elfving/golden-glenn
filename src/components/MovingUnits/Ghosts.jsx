29fa6904b2f5b4a24909a359cfdb00e7ebd5d046
Olivia Elfving <olivia.elfving@hotmail.com>
2022-10-31 14:39:11 +0100

    refactoring moving units

---
+++ src/components/MovingUnits/Ghosts.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+
+const Ghosts = () => (
+    <>
+        <path id="ghost-planePath" d="M1.972 40.922L319.27 27.814L550.275 27.692C550.275 27.692 867.041 44.695 867.487 44.801" fill="none" />
+        <path id="reverseGhost-path" d="M864.337 44.742L547.878 27.942L319.552 27.792L2.105 40.902" fill="none" />
+        
+        <g id="ghosts" opacity="0">
+            <g id="ghost-point-1">
+                <path fill="#275e8a" d="M8.25 55.669L8.28 31.951C10.262 11.869 38.257 12.046 40.052 32.084C40.057 32.143 40.056 55.457 40.056 55.457C39.039 58.945 36.855 53.716 34.709 54.116C33.792 54.287 33.201 55.051 32.608 55.867C31.427 57.494 30.621 59.505 29.074 59.197C28.306 59.045 27.317 57.287 26.323 55.803C25.456 54.509 24.037 53.497 23.338 54.301C19.984 58.161 20.683 59.455 18.293 59.104C16.694 58.869 14.968 53.531 13.482 54.047C11.75 54.648 10.659 56.012 9.791 56.533C9.094 56.95 8.479 56.761 8.25 55.669Z"/>
+                <ellipse fill="00002b" cx="18.216" cy="30.612" rx="3.4" ry="3.4" />
+                <ellipse fill="00002b" cx="30.216" cy="30.612" rx="3.4" ry="3.4" />
+            </g>
+            <g id="ghost-point-2">
+                <path fill="#275e8a" d="M8.25 55.669L8.28 31.951C10.262 11.869 38.257 12.046 40.052 32.084C40.057 32.143 40.056 55.457 40.056 55.457C39.039 58.945 36.855 53.716 34.709 54.116C33.792 54.287 33.201 55.051 32.608 55.867C31.427 57.494 30.621 59.505 29.074 59.197C28.306 59.045 27.317 57.287 26.323 55.803C25.456 54.509 24.037 53.497 23.338 54.301C19.984 58.161 20.683 59.455 18.293 59.104C16.694 58.869 14.968 53.531 13.482 54.047C11.75 54.648 10.659 56.012 9.791 56.533C9.094 56.95 8.479 56.761 8.25 55.669Z"/>
+                <ellipse fill="00002b" cx="18.216" cy="30.612" rx="3.4" ry="3.4" />
+                <ellipse fill="00002b" cx="30.216" cy="30.612" rx="3.4" ry="3.4" />
+            </g>
+            <g id="ghost-point-3">
+                <path fill="#275e8a" d="M8.25 55.669L8.28 31.951C10.262 11.869 38.257 12.046 40.052 32.084C40.057 32.143 40.056 55.457 40.056 55.457C39.039 58.945 36.855 53.716 34.709 54.116C33.792 54.287 33.201 55.051 32.608 55.867C31.427 57.494 30.621 59.505 29.074 59.197C28.306 59.045 27.317 57.287 26.323 55.803C25.456 54.509 24.037 53.497 23.338 54.301C19.984 58.161 20.683 59.455 18.293 59.104C16.694 58.869 14.968 53.531 13.482 54.047C11.75 54.648 10.659 56.012 9.791 56.533C9.094 56.95 8.479 56.761 8.25 55.669Z"/>
+                <ellipse fill="00002b" cx="18.216" cy="30.612" rx="3.4" ry="3.4" />
+                <ellipse fill="00002b" cx="30.216" cy="30.612" rx="3.4" ry="3.4" />
+            </g>
+            <g id="ghost-point-4" opacity="0">
+                <path fill="#275e8a" d="M8.25 55.669L8.28 31.951C10.262 11.869 38.257 12.046 40.052 32.084C40.057 32.143 40.056 55.457 40.056 55.457C39.039 58.945 36.855 53.716 34.709 54.116C33.792 54.287 33.201 55.051 32.608 55.867C31.427 57.494 30.621 59.505 29.074 59.197C28.306 59.045 27.317 57.287 26.323 55.803C25.456 54.509 24.037 53.497 23.338 54.301C19.984 58.161 20.683 59.455 18.293 59.104C16.694 58.869 14.968 53.531 13.482 54.047C11.75 54.648 10.659 56.012 9.791 56.533C9.094 56.95 8.479 56.761 8.25 55.669Z"/>
+                <ellipse fill="00002b" cx="18.216" cy="30.612" rx="3.4" ry="3.4" />
+                <ellipse fill="00002b" cx="30.216" cy="30.612" rx="3.4" ry="3.4" />
+            </g>
+        </g>
+
+        <g id="reverse_ghosts" opacity="0">
+            <g id="ghost-reverse_point-1" className="ghost-reverse">
+                <path fill="#3379af" d="M8.25 55.669L8.28 31.951C10.262 11.869 38.257 12.046 40.052 32.084C40.057 32.143 40.056 55.457 40.056 55.457C39.039 58.945 36.855 53.716 34.709 54.116C33.792 54.287 33.201 55.051 32.608 55.867C31.427 57.494 30.621 59.505 29.074 59.197C28.306 59.045 27.317 57.287 26.323 55.803C25.456 54.509 24.037 53.497 23.338 54.301C19.984 58.161 20.683 59.455 18.293 59.104C16.694 58.869 14.968 53.531 13.482 54.047C11.75 54.648 10.659 56.012 9.791 56.533C9.094 56.95 8.479 56.761 8.25 55.669Z"/>
+                <ellipse fill="00002b" cx="18.216" cy="30.612" rx="3.4" ry="3.4" />
+                <ellipse fill="00002b" cx="30.216" cy="30.612" rx="3.4" ry="3.4" />
+            </g>
+            <g id="ghost-reverse_point-2" className="ghost-reverse">
+                <path fill="#3379af" d="M8.25 55.669L8.28 31.951C10.262 11.869 38.257 12.046 40.052 32.084C40.057 32.143 40.056 55.457 40.056 55.457C39.039 58.945 36.855 53.716 34.709 54.116C33.792 54.287 33.201 55.051 32.608 55.867C31.427 57.494 30.621 59.505 29.074 59.197C28.306 59.045 27.317 57.287 26.323 55.803C25.456 54.509 24.037 53.497 23.338 54.301C19.984 58.161 20.683 59.455 18.293 59.104C16.694 58.869 14.968 53.531 13.482 54.047C11.75 54.648 10.659 56.012 9.791 56.533C9.094 56.95 8.479 56.761 8.25 55.669Z"/>
+                <ellipse fill="00002b" cx="18.216" cy="30.612" rx="3.4" ry="3.4" />
+                <ellipse fill="00002b" cx="30.216" cy="30.612" rx="3.4" ry="3.4" />
+            </g>
+            <g id="ghost-reverse_point-3" className="ghost-reverse">
+                <path fill="#3379af" d="M8.25 55.669L8.28 31.951C10.262 11.869 38.257 12.046 40.052 32.084C40.057 32.143 40.056 55.457 40.056 55.457C39.039 58.945 36.855 53.716 34.709 54.116C33.792 54.287 33.201 55.051 32.608 55.867C31.427 57.494 30.621 59.505 29.074 59.197C28.306 59.045 27.317 57.287 26.323 55.803C25.456 54.509 24.037 53.497 23.338 54.301C19.984 58.161 20.683 59.455 18.293 59.104C16.694 58.869 14.968 53.531 13.482 54.047C11.75 54.648 10.659 56.012 9.791 56.533C9.094 56.95 8.479 56.761 8.25 55.669Z"/>
+                <ellipse fill="00002b" cx="18.216" cy="30.612" rx="3.4" ry="3.4" />
+                <ellipse fill="00002b" cx="30.216" cy="30.612" rx="3.4" ry="3.4" />
+            </g>
+            <g id="ghost-reverse_point-4" className="ghost-reverse">
+                <path fill="#3379af" d="M8.25 55.669L8.28 31.951C10.262 11.869 38.257 12.046 40.052 32.084C40.057 32.143 40.056 55.457 40.056 55.457C39.039 58.945 36.855 53.716 34.709 54.116C33.792 54.287 33.201 55.051 32.608 55.867C31.427 57.494 30.621 59.505 29.074 59.197C28.306 59.045 27.317 57.287 26.323 55.803C25.456 54.509 24.037 53.497 23.338 54.301C19.984 58.161 20.683 59.455 18.293 59.104C16.694 58.869 14.968 53.531 13.482 54.047C11.75 54.648 10.659 56.012 9.791 56.533C9.094 56.95 8.479 56.761 8.25 55.669Z"/>
+                <ellipse fill="00002b" cx="18.216" cy="30.612" rx="3.4" ry="3.4" />
+                <ellipse fill="00002b" cx="30.216" cy="30.612" rx="3.4" ry="3.4" />
+            </g>
+        </g>
+
+        <animateMotion xlinkHref="#ghost-point-1" dur="9.5s" begin="0s" repeatCount="indefinite" rotate="auto">
+        <mpath xlinkHref="#ghost-planePath" />
+        </animateMotion>
+        <animateMotion xlinkHref="#ghost-point-2" dur="10s" begin="3s" repeatCount="indefinite" rotate="auto">
+            <mpath xlinkHref="#ghost-planePath" />
+        </animateMotion>
+        <animateMotion xlinkHref="#ghost-point-3" dur="10s" begin="5.5s" repeatCount="indefinite" rotate="auto">
+            <mpath xlinkHref="#ghost-planePath" />
+        </animateMotion>
+        <animateMotion xlinkHref="#ghost-point-4" dur="9.5s" begin="7.5s" repeatCount="indefinite" rotate="auto">
+            <mpath xlinkHref="#ghost-planePath" />
+        </animateMotion>
+
+
+        <animate attributeName="opacity"
+            xlinkHref="#ghosts"
+            values="0; 1;" dur="1s" begin="6"
+            data-id="ghosts"
+            repeatCount="1"
+            fill="freeze"
+        />
+
+        <animate attributeName="opacity"
+            xlinkHref="#ghost-point-4"
+            values="0; 1;" dur="0.1s" begin="7.5"
+            data-id="ghosts"
+            repeatCount="1"
+            fill="freeze"
+        />
+                
+        <animateMotion xlinkHref="#ghost-reverse_point-1" dur="8.8s" begin="0s" repeatCount="indefinite" rotate="auto">
+            <mpath xlinkHref="#reverseGhost-path" />
+        </animateMotion>
+        <animateMotion xlinkHref="#ghost-reverse_point-2" dur="10s" begin="2.5s" repeatCount="indefinite" rotate="auto">
+            <mpath xlinkHref="#reverseGhost-path" />
+        </animateMotion>
+        <animateMotion xlinkHref="#ghost-reverse_point-3" dur="10s" begin="5s" repeatCount="indefinite" rotate="auto">
+            <mpath xlinkHref="#reverseGhost-path" />
+        </animateMotion>
+        <animateMotion xlinkHref="#ghost-reverse_point-4" dur="10s" begin="7.5s" repeatCount="indefinite" rotate="auto">
+            <mpath xlinkHref="#reverseGhost-path" />
+        </animateMotion>
+        <animateMotion xlinkHref="#ghost-reverse_point-5" dur="8.8s" begin="11s" repeatCount="indefinite" rotate="auto">
+            <mpath xlinkHref="#reverseGhost-path" />
+        </animateMotion>
+
+        <animate attributeName="opacity"
+            xlinkHref="#reverse_ghosts"
+            values="0; 1;" dur="1s" begin="6"
+            data-id="ghosts"
+            repeatCount="1"
+            fill="freeze"
+        />
+    </>
+)
+
+export default Ghosts;
