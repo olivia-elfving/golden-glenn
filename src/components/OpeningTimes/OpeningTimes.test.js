8330b573e9f47ec0cfd5d1fd84de7aec1e27e864
Joakim Johansson <johansson.joakim@gmail.com>
2022-11-18 15:10:34 +0100

    Clean up tests

--- src/components/OpeningTimes/OpeningTimes.test.js
+++ src/components/OpeningTimes/OpeningTimes.test.js
@@ -9,7 +9,7 @@ const commonTimeText = 'Vanlig tid för broöppning:';
 describe('Next time component', () => {
 
     test('show next opening when time is 05:00', async () => {
-        const time = new Date('20 Aug 2020 05:00:00 GMT+2').getTime();
+        const time = new Date('20 Aug 2020 05:00:00').getTime();
         jest.useFakeTimers('modern');
         jest.setSystemTime(time);
         render(<OpeningTimes />);
@@ -20,7 +20,7 @@ describe('Next time component', () => {
     });
 
     test('show next opening when time is 09:00', async () => {
-        const time = new Date('20 Aug 2020 09:00:00 GMT+2').getTime();
+        const time = new Date('20 Aug 2020 09:00:00').getTime();
         jest.useFakeTimers('modern');
         jest.setSystemTime(time);
         render(<OpeningTimes />);
@@ -31,14 +31,14 @@ describe('Next time component', () => {
     });
 
     test('show next opening when time is 09:30', async () => {
-        const time = new Date('20 Aug 2020 09:30:00 GMT+2').getTime();
+        const time = new Date('20 Aug 2020 09:30:00').getTime();
         jest.useFakeTimers('modern');
         jest.setSystemTime(time);
         render(<OpeningTimes />);
 
         await screen.getByTestId('next_time');
 
-        expect(screen.getByTestId('next_time')).toHaveTextContent(`${nextPlannedTimeText} 05:35`);
+        expect(screen.getByTestId('next_time')).toHaveTextContent(`${nextPlannedTimeText} 09:35`);
     });
 
 });
